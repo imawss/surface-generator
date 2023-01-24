63ff68a0c6d9c416a28085babe5b783b322942d4
Aras Arkan <83475962+ArasArkan@users.noreply.github.com>
2023-01-24 22:24:45 +0300

    Inıtial commit!

---
+++ sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(600, 600, WEBGL);
+  noiseDetail(0.5);
+  angleMode(DEGREES);
+}
+
+function draw() {
+  
+  var boxVolume = 5;
+  var offsetX = 0;
+  var offsetY = 0;
+  
+  background(0, 0, 0);
+  rotateX(45);
+  
+  for(var w = -width/4; w <= width/4; w += boxVolume){
+    offsetY=0;
+    for(var h = -height/4; h<= height/4; h += boxVolume){
+      var noiseSurface = map(noise(offsetX, offsetY), 0, 1, -50, 50);
+      
+      push();
+      translate(w,h,noiseSurface);
+      box(boxVolume);
+      pop();
+  
+      offsetY +=0.1;
+    }
+    offsetX += 0.1;
+  }
+  
+}
